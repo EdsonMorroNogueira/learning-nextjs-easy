832b75e805f360539ab5d72d928726ff1b8e8d83
Edson Morro Nogueira <125297431+EdsonMorroNogueira@users.noreply.github.com>
2024-09-09 15:43:45 -0300

    feat(Parei no Chapter13)

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,59 @@
+import Cards, { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchCardData } from "@/app/lib/data";
+import { Suspense } from "react";
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+  CardsSkeleton,
+} from "@/app/ui/skeletons";
+import CardWrapper from "@/nextjs-dashboard/app/ui/dashboard/cards";
+
+export default async function Page() {
+  // !REQUEST WATERFALL
+
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices(); // !wait for fetchRevenue() to finish
+  // const {
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  //   numberOfInvoices,
+  //   numberOfCustomers,
+  // } = await fetchCardData(); // !wait for fetchLatestInvoices() to finish
+
+  /* Static rendering is useful for UI with no data or data that is shared across users, 
+  such as a static blog post or a product page. 
+  It might not be a good fit for a dashboard that has personalized data which is regularly updated.
+  */
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* SUSPENSE É MUITO IMPORTANTE */}
+        <Suspense fallback={<CardsSkeleton />}>
+          <Cards />
+        </Suspense>
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
